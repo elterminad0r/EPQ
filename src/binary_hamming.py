1594722e7d56cce5529fde2e702c42733d793e7e
Izaak van Dongen <izaak.van.dongen@gmail.com>
2018-05-12 20:02:23 +0100

    Change things to be figures and used references instead of colons

--- src/binary_hamming.py
+++ src/binary_hamming.py
@@ -1,19 +1,25 @@
-#!/usr/bin/env python3
+#4!/usr/bin/env python3
 
 
 """
 Hamming encoding framework for binary objects, using even parity.
 """
 
+# imports the "count" and "takewhile" functions
 from itertools import count, takewhile
 
+# function to get all of the powers of 2 up to a given upper limit.
+# Uses count() to produce the set of natural numbers (0, 1, 2, 3..)
+# and takewhile() to keep taking powers of 2 until they exceed the limit.
 def powers_to(n):
     return takewhile(lambda x: x < n, (1 << i for i in count()))
 
+# function that generates the particular indices covered by a parity bit
 def matching_indices(power, l):
     return (i for pstart in range(power - 1, l, power << 1)
               for i in range(pstart, min(l, pstart + power)))
 
+# function to Hamming encode a series of bits.
 def hamming_encode(bin_stream):
     pwr = 1
     out = []
--- src/test_binary_hamming.py
+++ src/test_binary_hamming.py
@@ -1,5 +1,8 @@
 """
-Unit tests for binary_hamming.py
+Unit tests for binary_hamming.py.
+
+This is a testing program, that runs a series of test cases to verify that my
+code works.
 """
 
 import unittest
@@ -7,6 +10,7 @@ import unittest
 from binary_hamming import powers_to, hamming_encode
 
 class BinaryHammingTestCase(unittest.TestCase):
+    # testing the "powers_to" function
     def test_powers_to(self):
         self.assertEqual(list(powers_to(0)), [])
         self.assertEqual(list(powers_to(1)), [])
@@ -15,6 +19,7 @@ class BinaryHammingTestCase(unittest.TestCase):
         self.assertEqual(list(powers_to(5)), [1, 2, 4])
         self.assertEqual(list(powers_to(13)), [1, 2, 4, 8])
 
+    # testing the "hamming_encode" function
     def test_hamming_encode(self):
         self.assertEqual(hamming_encode([1, 0, 1, 1]), [0, 1, 1, 0, 0, 1, 1])
         self.assertEqual(hamming_encode(
